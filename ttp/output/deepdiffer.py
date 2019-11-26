c490ec774ed52343685a8a683403d65bb5073806
Denis <d.mulyalin@gmail.com>
2019-11-26 18:22:02 +1100

    added support for addfield and extra kwargs for deepdiff function

--- ttp/output/deepdiffer.py
+++ ttp/output/deepdiffer.py
@@ -13,12 +13,14 @@ _name_map_ = {
 }
 
 
-def deepdiff_func(data, before, after, keep_results=False):
+def deepdiff_func(data, before, after, add_field=False, **kwargs):
     """
     Function to compare two structures.
     
     * before - name of input that contains old data
     * after - name of input that contains new data
+	* add_key - name of the key to add to data instead of replacing it
+	* kwargs - arguments supported by deepdiff DeepDiff class e.g. ignore_order or verbose_level
     """
     if not isinstance(data, list):
         return data
@@ -30,9 +32,10 @@ def deepdiff_func(data, before, after, keep_results=False):
     before_index = list(template_obj.inputs.keys()).index(before)
     after_index = list(template_obj.inputs.keys()).index(after)
     # run compare
-    result = DeepDiff(data[before_index], data[after_index], ignore_order=True, verbose_level=2)
-    if keep_results:
-        data.append({'deepdiff': result})
+    result = DeepDiff(data[before_index], data[after_index], **kwargs)
+    # return results
+    if add_field:
+        data.append({add_field: result})
         return data
     else:
         return result
