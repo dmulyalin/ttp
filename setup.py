cd78293e23b7fef08ab7edc7248b7df3db1b4eca
Denis <d.mulyalin@gmail.com>
2019-12-22 18:41:08 +1100

    updated setup file version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __author__ = "Denis Mulyalin <d.mulyalin@gmail.com>"
 
 setup(
     name="ttp",
-    version="0.2.0",
+    version="0.3.0",
     author="Denis Mulyalin",
     author_email="d.mulyalin@gmail.com",
     description="Template Text Parser",
