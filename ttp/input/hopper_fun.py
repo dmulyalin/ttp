a725053389de97364c322e7771b9c36376de911e
denis <d.mulyalin@gmail.com>
2020-07-05 20:39:18 +1000

    updated hopper input function to support updatedhopper module

--- ttp/input/hopper_fun.py
+++ ttp/input/hopper_fun.py
@@ -7,28 +7,20 @@ _name_map_ = {
 "hopper_fun": "hopper"
 }
 
-def hopper_fun(input_name, **kwargs):
-    devices = kwargs.get("devices", [])
-    commands = kwargs.get("commands", [])
-    credentials = kwargs.get("credentials", [])
-    max_connections = kwargs.get("max_connections", 1)
-    process_output = kwargs.get("process_output", "pyte")
-    username = kwargs.get("username", None)
-    password = kwargs.get("password", None)
-    if username == "get_user_input":
+def hopper_fun(input_name, **all_kwargs):
+    # remove known TTP kwargs:
+    ttp_known_kwargs = ['load', 'extensions', 'filters', 'urls', 'source', 'groups', 'name'] + list(_ttp_["input"].keys())
+    kwargs = {k: v for k, v in all_kwargs.items() if not k in ttp_known_kwargs}
+    #get username and password
+    if kwargs.get("username", "") == "get_user_input":
         username = input('{}, enter username: '.format(input_name))
-    if password == "get_user_pass":
+    if kwargs.get("password", "") == "get_user_pass":
         password = getpass.getpass('{}, enter password: '.format(input_name))
-    credentials.append((username, password,))
-    log.info("TTP hopper_fun: sending - '{}', to - '{}'".format(commands, devices))
+    kwargs.setdefault("credentials", [])
+    kwargs["credentials"].append((username, password,))
+    log.info("TTP input hopper_fun: sending - '{}', to - '{}'".format(kwargs.get("commands"), kwargs.get("devices")))
     # get data from devices
     hopperObj = hopper()
-    result = hopperObj.run(
-        devices=devices, 
-        commands=commands, 
-        credentials=credentials, 
-        max_connections=max_connections, 
-        process_output=process_output
-    )
-    log.info("TTP hopper_fun: received output from {} devices".format(len(result.keys())))
+    result = hopperObj.run(**kwargs)
+    log.info("TTP input hopper_fun: received output from {} devices".format(len(result.keys())))
     return list(result.values())
